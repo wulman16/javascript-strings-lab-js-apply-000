3751c3eeba17c80167d605e2c941136fe3c4078c
William Ulman <will.ulman@gmail.com>
2019-01-02 20:13:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `Let's talk about ${topic}.`;
+=======
+var greetSpecialGuest = "Hello," + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = `${topic}`;
+>>>>>>> 4538a40ec9ac33ec29768bfdbe4a8442811f95a0
